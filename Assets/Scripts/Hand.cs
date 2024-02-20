f6428854cfdf9ae7e49d4abb0a8fdb122a2b98df
Peter Balling <peter.balling@gmail.com>
2024-02-20 18:49:12 +0100

    Init commit

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    public float speed;
+    Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+
+  // Start is called before the first frame update
+  void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    internal void SetGrip(float value)
+    {
+        gripTarget = value;
+    }
+
+    internal void SetTrigger(float value)
+    {
+        triggerTarget = value;
+    }
+
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat("Grip", gripCurrent);
+        }
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat("Trigger", triggerCurrent);
+        }
+    }
+
+
+}
